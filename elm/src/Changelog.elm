55236770b82167e4e416d4e3a1f41a42fa386777
Thomas Dendale <thomas.dendale@blooloc.com>
2023-12-11 18:18:39 +0100

    Add more words

--- elm/src/Changelog.elm
+++ elm/src/Changelog.elm
@@ -18,7 +18,12 @@ currentVersion =
 
 
 changelog =
-    [ ( Date.fromCalendarDate 2023 Dec 8
+    [ ( Date.fromCalendarDate 2023 Dec 11
+      , "0.3.4"
+      , [ "Added 9 more CLT5 semester 1 Extra words"
+        ]
+      )
+    , ( Date.fromCalendarDate 2023 Dec 8
       , "0.3.3"
       , [ "Added Ctrl+Enter keyboard shortcut, to more easily give up and go to the next word"
         ]
